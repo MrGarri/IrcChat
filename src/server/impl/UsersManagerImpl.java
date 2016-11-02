528bf42a6d8dc8d11d6cee6a30ad15d506c7ca84
Roberto Garrido <garrihendrix@gmail.com>
2016-11-02 11:55:07 +0100

    addUser implemented

--- src/server/impl/UsersManagerImpl.java
+++ src/server/impl/UsersManagerImpl.java
@@ -1,4 +1,32 @@
 package server.impl;
 
-public class UsersManagerImpl {
+import common.User;
+import server.UsersManager;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsersManagerImpl implements UsersManager{
+
+    Map<String, User> users = new HashMap<>();
+
+    @Override
+    public boolean userExists(String username) {
+        return users.containsKey(username);
+    }
+
+    @Override
+    public User addUser(String username, String password) {
+
+        User user = new User(username, password);
+        users.put(username, user);
+
+        return user;
+
+    }
+
+    @Override
+    public boolean login(String username, String password) {
+        return false;
+    }
 }
