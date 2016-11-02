b66d2f019de3086fa536cc133d9bed376157fa1f
Roberto Garrido <garrihendrix@gmail.com>
2016-11-02 12:12:32 +0100

    equals implemented
    # Conflicts:
    #	src/common/User.java

---
+++ src/common/User.java
@@ -0,0 +1,40 @@
+package common;
+
+public class User {
+
+    public String user;
+    public String password;
+
+    public User(String user, String password) {
+        this.user = user;
+        this.password = password;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user1 = (User) o;
+
+        if (user != null ? !user.equals(user1.user) : user1.user != null) return false;
+        return password != null ? password.equals(user1.password) : user1.password == null;
+
+    }
+}
