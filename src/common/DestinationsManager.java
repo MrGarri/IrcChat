4cb8450602b99c7d09ea4ef185c6e78e3d1e3bb2
Roberto Garrido <garrihendrix@gmail.com>
2016-11-05 17:43:09 +0100

    location changed

--- src/common/DestinationsManager.java
+++ src/common/DestinationsManager.java
@@ -1,20 +1,34 @@
-package server;
+package common;
 
 import common.CommonDestinations;
 
-import javax.jms.Destination;
+import javax.jms.JMSContext;
+import javax.jms.Queue;
 import javax.jms.Topic;
 
 
 public class DestinationsManager implements CommonDestinations {
+
+    JMSContext context;
+
+    Queue requestsQueue;
+
+    public DestinationsManager(JMSContext context){
+        this.context = context;
+    }
+
+    @Override
+    public Queue getRequestsQueue(){
+        if(requestsQueue == null)
+            context.createQueue(REQUESTS);
+
+        return requestsQueue;
+    }
+
     @Override
     public Topic getRoomsTopic() {
         //TODO
         return null;
     }
 
-    public Destination getReplyDestination(){
-        //TODO
-        return null;
-    }
 }
