39a4e787d2a775df757fc3556ffc5821d7d07725
Pablo Garcia <yinxro@gmail.com>
2016-11-05 19:29:03 +0100

    Basic client implementations

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,4 +1,9 @@
 package client;
 
+import common.CommonDestinations;
+import common.ServerRequest;
+
 public interface Client {
+    void makeRequest(ServerRequest request, RequestCallback callback);
+    CommonDestinations getDestinationsManager();
 }
--- src/client/ClientImpl.java
+++ src/client/ClientImpl.java
@@ -2,19 +2,67 @@ package client;
 
 import client.base.Presenter;
 import client.login.impl.LoginPresenterImpl;
+import common.CommonDestinations;
+import common.DestinationsManager;
+import common.ServerRequest;
+import common.ServerResponse;
 
-import javax.jms.ConnectionFactory;
-import javax.jms.JMSContext;
+import javax.jms.*;
 
-public class ClientImpl implements Client {
+public class ClientImpl implements Client, MessageListener {
 
-    JMSContext context;
+    private JMSContext context;
+    private CommonDestinations destinationsManager;
+
+    private Queue responseQueue;
+    private RequestCallback callback;
+    private User loggedUser;
 
     public ClientImpl(JMSContext context){
         this.context = context;
+        destinationsManager = new DestinationsManager(context);
+    }
+
+    @Override
+    public void makeRequest(ServerRequest request, RequestCallback callback){
+        try {
+            ObjectMessage message = context.createObjectMessage();
+            message.setObject(request);
+
+            context.createProducer().send(getDestinationsManager().getRequestsQueue(), message);
+            this.callback = callback;
+        } catch (JMSException e) {
+            callback.failure(null);
+        }
+    }
+
+    @Override
+    public void onMessage(Message message) {
+        if(callback != null) {
+            try {
+                ServerResponse response = message.getBody(ServerResponse.class);
+                if (response.wasSuccessful()) {
+                    callback.success(response);
+                } else {
+                    callback.failure(response);
+                }
+            } catch (JMSException e) {
+                callback.failure(null);
+            }
+
+            callback = null;
+        }
+    }
+
+    @Override
+    public CommonDestinations getDestinationsManager() {
+        return destinationsManager;
     }
 
     private void run(){
+        responseQueue = context.createQueue(System.getProperty("user.name"));
+        context.createConsumer(responseQueue).setMessageListener(this);
+
         Presenter presenter = new LoginPresenterImpl();
         presenter.initialize(this);
     }
