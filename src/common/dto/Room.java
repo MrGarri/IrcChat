1e841bd251ef10f4e15ccdc18ead877c4893ae6a
Pablo Garcia <yinxro@gmail.com>
2016-11-15 17:32:54 +0100

    Implemented equals an dhashcode in room

--- src/common/dto/Room.java
+++ src/common/dto/Room.java
@@ -32,4 +32,19 @@ public class Room implements Serializable {
     public String toString() {
         return getName();
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Room room = (Room) o;
+
+        return name != null ? name.equals(room.name) : room.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
 }
