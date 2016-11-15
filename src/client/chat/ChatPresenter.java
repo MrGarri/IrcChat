4c17499a93e9d93b6a5ad6433a03a0968303ac79
Diego Fernandez Peces-Barba <d.fernandezp@alumnos.upm.es>
2016-11-15 16:46:45 +0100

    Added getRoom to obtain the roomName.

--- src/client/chat/ChatPresenter.java
+++ src/client/chat/ChatPresenter.java
@@ -1,10 +1,12 @@
 package client.chat;
 
 import client.base.Presenter;
+import common.dto.Room;
 
 public interface ChatPresenter extends Presenter<ChatView> {
 
     void leaveRoom();
     void sendMessage(String message);
+    Room getRoom();
 
 }
